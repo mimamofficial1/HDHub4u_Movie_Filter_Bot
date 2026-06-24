192996758ace6fc52acd7edff446b97a6ad05f16
mimamofficial1 <muzaffarimamneema@gmail.com>
2026-06-24 23:31:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -210,7 +210,7 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://mrn-universe-filter-bot-stream.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else "https://mrn-universe-filter-bot-stream.koyeb.app/".format(FQDN, PORT)
+URL = "https://hdhub4u-movie-filter-bot.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else "https://hdhub4u-movie-filter-bot.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'Mrn_Officialx'))
@@ -224,9 +224,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "https://mrn-universe-filter-bot-stream.koyeb.app/".format(FQDN)
+    URL = "https://hdhub4u-movie-filter-bot.koyeb.app/".format(FQDN)
 else:
-    URL = "https://mrn-universe-filter-bot-stream.koyeb.app/".format(FQDN)
+    URL = "https://hdhub4u-movie-filter-bot.koyeb.app/".format(FQDN)
 
 # ============================
 # Reactions Configuration
